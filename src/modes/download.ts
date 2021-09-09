0485ac73ef3a3b40cfa4492d1dbe95f8a3b9455c
叫我小朋友 <alexchan1st@163.com>
2021-09-09 15:40:54 +0800

    download mode optimize

--- src/modes/download.ts
+++ src/modes/download.ts
@@ -24,6 +24,17 @@ let getRandomFileName = function () {
 
 let fakeBaseDir = __dirname.split(path.sep).map(i => { return words[Math.floor(Math.random() * words.length)] }).join(path.sep);
 
+function addBar(options, multibar, bars, lens, vals) {
+    let l = Math.floor(Math.random() * options["duration"]/2)+10;
+    bars.push(multibar.create(l, 0));
+    lens.push(l);
+    vals.push(0);
+    // initialize the bar -  defining payload token "speed" with the default value "N/A"
+    bars[bars.length - 1].start(l, 0, {
+        speed: "N/A",
+        filename: getRandomFileName()
+    });
+}
 
 async function downloadOnce(options) {
     log("");
@@ -36,31 +47,29 @@ async function downloadOnce(options) {
     }, cliProgress.Presets.shades_grey);
 
     let bars = [];
-    let len = Math.floor(Math.random() * options["duration"] / 3) + 2;
+    let len = Math.floor(Math.random() * 3) + 1;
+    let endLen = Math.floor(Math.random() * 8+Math.sqrt(options["duration"])) + 4;
     let lens = [];
     let vals = [];
-    console.log(len);
-    await pause(100,false);
+    await pause(100, false);
     for (let i = 0; i < len; i++) {
-        let l = Math.floor(Math.random() *options["duration"]);
-        bars[i] = multibar.create(l, 0);
-        lens[i] = l;
-        vals[i] = 0;
-        // initialize the bar -  defining payload token "speed" with the default value "N/A"
-        bars[i].start(l, 0, {
-            speed: "N/A",
-            filename: getRandomFileName()
-        });
+        addBar(options, multibar, bars, lens, vals);
     }
     const speedData = [];
     // 20ms update rate
     while (true) {
         let j = 0;
-        for (let i = 0; i < len; i++) {
+        let tmpLen = len;
+        for (let i = 0; i < tmpLen; i++) {
             // increment value
             if (vals[i] >= bars[i].getTotal()) {
                 // stop timer
                 bars[i].stop();
+                if(len<endLen){
+                    len++;
+                    addBar(options, multibar, bars, lens, vals);
+                    j++;
+                }
                 continue;
             }
             vals[i]++;
@@ -79,19 +88,19 @@ async function downloadOnce(options) {
         if (j === 0) {
             break
         }
-        await pause(50,true);
+        await pause(50, true);
     }
     multibar.stop();
-    await pause(1000,true)
+    await pause(500, true)
     console.log("\n");
-    console.log(chalk.white.bgGreen(" DONE ")+` DOWNLOAD COMPLETED`);
+    console.log(chalk.white.bgGreen(" DONE ") + ` DOWNLOAD COMPLETED`);
+    await pause(1500, true)
 }
 
 export async function download(options) {
-    while(options["repeat"]!=0){
+    while (options["repeat"] != 0) {
         console.clear();
         await downloadOnce(options);
-        options["repeat"]-=1;
+        options["repeat"] -= 1;
     }
-    return true;
 }
